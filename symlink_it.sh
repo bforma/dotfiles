9b3f1de304c001e093401b19f3cb249658d60219
Bob Forma <bobforma@gmail.com>
2014-09-08 10:20:17 +0200

    Setup tmuxinator

--- symlink_it.sh
+++ symlink_it.sh
@@ -7,7 +7,7 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-files="gemrc gitconfig git_template irbrc rspec vim vimrc zshrc"    # list of files/folders to symlink in homedir
+files="gemrc gitconfig git_template irbrc rspec vim vimrc zshrc tmuxinator"    # list of files/folders to symlink in homedir
 
 ##########
 
